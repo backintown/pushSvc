e230385f131bc99a734ef1077b508e8c7beb5795
Alex Cheng <cheng.alex5@gmail.com>
2018-06-08 14:27:39 +0800

    save trx before sending, fix query logic

--- api/routes/notifications.js
+++ api/routes/notifications.js
@@ -3,14 +3,16 @@ const router = express.Router();
 const Device = require('../models/Device');
 const App = require('../models/App')
 const Transaction = require('../models/Transaction');
-const Log = require('../models/Log');
 const APNS = require('../protocols/apns');
 const FCM = require('../protocols/fcm');
 
-//todo - notification data option for fcm
-//todo - change save transaction order
+
+// send to user
 //todo - make transactions async
 router.post('/account', (req, res, next) => {
+  if (!req.body.accountId || !req.body.appId || !req.body.osPlatform)
+    return res.status(400).json({ err: "Missing params" });
+
   Device.find({ accountId: req.body.accountId, appId: req.body.appId, osPlatform: req.body.osPlatform })
     .exec()
     .then(devices => {
@@ -20,7 +22,10 @@ router.post('/account', (req, res, next) => {
         })
       }
       // find app uid for each version
-      const appUID = App.find({ appId: req.body.appId, osPlatform: req.body.osPlatform }).exec().then(result => result[0]._id);
+      const appUID = App.find({ appId: req.body.appId, osPlatform: req.body.osPlatform })
+        .exec()
+        .then(result => result[0]._id)
+        .catch(err => { console.log(err) });
       // save transaction
       const trxId = saveTransaction({
         message: req.body.notification.message,
@@ -42,109 +47,77 @@ router.post('/account', (req, res, next) => {
     .catch(err => { res.status(500).json({ err }) });
 });
 
-//todo - change logic to include os platform
+
+// send to all 
 router.post('/app', (req, res, next) => {
+  if (!req.body.appId || !req.body.osPlatform)
+    return res.status(400).json({ err: "Missing params" });
+
+  const topic = req.body.appId;
   Device.find({ appId: req.body.appId, osPlatform: req.body.osPlatform })
     .exec()
     .then(devices => {
       if (devices.length < 1) {
         return res.status(404).json({
-          message: "No devices exist with that app."
+          message: "No devices exist for that app."
         })
-      } else {
-        //construct token arrays for iOS and android
-        const iOSDevices = devices.filter(device => {
-          device.osPlatform == 'iOS'
-        });
-        const androidDevices = devices.filter(device => {
-          device.osPlatform == 'android'
-        });
-        //get app uid for each platform
-        const androidAppId = App.find({ appId: req.body.appId, osPlatform: 'android' }).exec().then(result => result[0]._id);
-        const iOSAppId = androidAppId = App.find({ appId: req.body.appId, osPlatform: 'iOS' }).exec().then(result => result[0]._id);
-        //send message
-        FCM.send(req.body.notification, androidDevices, androidAppId)
-          .then(result => {
-            saveTransaction({
-              message: req.body.notification.message,
-              title: req.body.notification.title,
-              appId: androidAppId,
-              osPlatform: 'android',
-              deviceId: androidDevices.map(device => {
-                return device._id;
-              }),
-              pushId: androidDevices.map(device => {
-                return device.pushId;
-              })
-            });
-            return res.status(200).json({ result });
-          });
-        APNS.send(req.body.notification, iOSDevices, iOSAppId)
-          .then(result => {
-            saveTransaction({
-              message: req.body.notification.message,
-              title: req.body.notification.title,
-              appId: iOSAppId,
-              osPlatform: 'iOS',
-              deviceId: iOSDevices.map(device => {
-                return device._id;
-              }),
-              pushId: iOSDevices.map(device => {
-                return device.pushId;
-              })
-            });
-            return res.status(200).json({ result });
-          });
       }
-    });
+      // find app uid for each version
+      const appUID = App.find({ appId: req.body.appId, osPlatform: req.body.osPlatform })
+        .exec()
+        .then(result => result[0]._id)
+        .catch(err => { console.log(err) });
+      // save transaction
+      const trxId = saveTransaction({
+        message: req.body.notification.message,
+        title: req.body.notification.title,
+        appId: req.body.appId,
+        osPlatform: req.body.osPlatform,
+        deviceId: devices,
+        pushId: devices.map(device => {
+          return device.pushId;
+        })
+      });
+      //send message
+      req.body.osPlatform === 'iOS' ?
+        APNS.send(req.body.notification, devices, appUID, trxId) :
+        FCM.sendTopic(req.body.notification, topic, appUID, trxId);
+      return res.status(200).json({ trxId }); // return trxId
+    })
+    .catch(err => { res.status(500).json({ err }) });
 });
 
-//change logic to include appId and os platform
+
+// send to device
 router.post('/device', (req, res, next) => {
-  Device.find({ _id: req.body.deviceId })
+  Device.find({ serialNumber: req.body.serialNumber })
     .exec()
-    .then(device => {
-      if (device.length < 1) {
+    .then(result => {
+      if (result.length < 1) {
         return res.status(404).json({
           message: "No device found."
         })
-      } else {
-        // get app uid
-        const appId = App.find({ appId: req.body.appId, osPlatform: device[0].osPlatform }).exec().then(result => {
-          if (result.length < 1)
-            return res.json(404).json({
-              message: "No devices exist for that app or no app for that platform"
-            })
-          return result[0]._id;
-        });
-        //send message using fcm or apns depending on device platform
-        device[0].osPlatform === 'android' ?
-          FCM.send(req.body.notification, device[0], appId)
-            .then(result => {
-              saveTransaction({
-                message: req.body.notification.message,
-                title: req.body.notification.title,
-                appId: androidAppId,
-                osPlatform: 'android',
-                deviceId: device[0]._id,
-                pushId: device[0].pushId
-              });
-              return res.status(200).json({ result });
-            })
-          :
-          APNS.send(req.body.notification, device[0], appId)
-            .then(result => {
-              saveTransaction({
-                message: req.body.notification.message,
-                title: req.body.notification.title,
-                appId: iOSAppId,
-                osPlatform: 'iOS',
-                deviceId: device[0]._id,
-                pushId: device[0].pushId
-              });
-              return res.status(200).json({ result });
-            });
       }
+      const device = result[0];
+      // get app uid
+      const appUID = App.find({ appId: req.body.appId, osPlatform: device.osPlatform })
+        .exec()
+        .then(result => result[0]._id)
+        .catch(err => { console.log(err) });
+      // save transaction
+      const trxId = saveTransaction({
+        message: req.body.notification.message,
+        title: req.body.notification.title,
+        appId: req.body.appId,
+        osPlatform: req.body.osPlatform,
+        deviceId: device,
+        pushId: device.pushId
+      });
+      //send message using fcm or apns depending on device platform
+      device.osPlatform !== 'iOS' ?
+        FCM.send(req.body.notification, device, appUID, trx) :
+        APNS.send(req.body.notification, device, appUID, trx);
+      return res.status(200).json({ trxId });
     });
 });
 
