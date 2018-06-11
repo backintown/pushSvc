88bc5867e6fe906fd89682cfe192e01d0307c4c6
Alex Cheng <cheng.alex5@gmail.com>
2018-06-11 17:42:52 +0800

    remove deviceId pushId from trx

--- api/models/Transaction.js
+++ api/models/Transaction.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 
 const transactionSchema = mongoose.Schema({
-  appId: { type: String , required:true},
+  appId: { type: String, required: true },
   osPlatform: { type: String, required: true },
   accountId: { type: String },
-  deviceId: { type: [String] }, // store in log
-  pushId: { type: [String] },
   title: { type: String },
   message: { type: String },
   appointment: { type: Date, default: Date.now },
@@ -14,5 +12,5 @@ const transactionSchema = mongoose.Schema({
 
 });
 
-transactionSchema.index({ appId: 1  })
+transactionSchema.index({ appId: 1 })
 module.exports = mongoose.model('Transaction', transactionSchema);
